b49094027a3482bd627d79bf1ade2d743051ed08
Fadzlan15 <90334147+Fadzlan15@users.noreply.github.com>
2021-09-21 11:40:25 +0700

    Add files via upload

---
+++ Membuat Jajargenjang.py
@@ -0,0 +1,4 @@
+print("Jajargenjang")
+
+for i in range(5,0,-1):
+    print(' '*(i-1)+'*'*(5-i)+'*'*i)
---
+++ Membuat Segtiga kebawah.py
@@ -0,0 +1,5 @@
+print(" Membuat Segitiga Kebawah")
+
+A = 5
+for i in reversed (range(1, A+1)):
+    print(i*"*")
